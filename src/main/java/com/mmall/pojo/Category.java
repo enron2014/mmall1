cbf037b846a27b1aff9af66259b343e0956d63c1
李辉 <lihui@lihuideMacBook-Pro.local>
2019-12-11 17:42:50 +0800

    Initial commit

---
+++ src/main/java/com/mmall/pojo/Category.java
@@ -0,0 +1,31 @@
+package com.mmall.pojo;
+
+import lombok.*;
+
+import java.util.Date;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+
+public class Category {
+    private Integer id;
+
+    private Integer parentId;
+
+    private String name;
+
+    private Boolean status;
+
+    private Integer sortOrder;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+
+
+
+}
